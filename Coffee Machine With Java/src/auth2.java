4edfd7674f7cd5d738be673706210bd50fe65c61
Pedro <valenit@proton.me>
2025-09-25 11:39:08 +0200

    doble

--- Coffee Machine With Java/src/auth2.java
+++ Coffee Machine With Java/src/auth2.java
@@ -1,6 +1,8 @@
 public class auth2 {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo 2");
+        System.out.println("Hello World");
+        System.out.println("Hola mundo 2");
     }
 }
