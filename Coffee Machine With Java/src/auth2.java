f9c615b02df22224b924403dbf24a5a9c68decca
Pedro <valenit@proton.me>
2025-09-25 11:40:39 +0200

    Borrar

--- Coffee Machine With Java/src/auth2.java
+++ Coffee Machine With Java/src/auth2.java
@@ -2,7 +2,5 @@ public class auth2 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hola mundo 2");
-        System.out.println("Hello World");
-        System.out.println("Hola mundo 2");
     }
 }
